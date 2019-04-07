4b747b75e709fd9cb4d8dec08145957f15170a8d
drcrotato <31459944+drcrotato@users.noreply.github.com>
2019-04-07 14:11:05 -0400

    Got the map totally tweaked out

--- main.js
+++ main.js
@@ -5,21 +5,29 @@ c.width = document.body.clientWidth; // make canvas full screen
 c.height = document.body.clientHeight;
 // add an off screen canvas for image rotation and such
 const osC = document.createElement('canvas');
-osC.width = document.body.clientWidth;
-osC.height = document.body.clientWidth;
+osC.width = 15500;
+osC.height = 7500;
 osC.style.position = "absolute";
 osC.id = 'off-screen-canvas';
 osC.hidden = false;
 const osDraw = osC.getContext('2d');
 document.body.appendChild(osC);
+const osC2 = document.createElement('canvas');
+osC2.width = 8000;
+osC2.height = 8000;
+osC2.style.position = "absolute";
+osC2.id = 'off-screen-canvas-2';
+osC2.hidden = false;
+const osDraw2 = osC2.getContext('2d');
+document.body.appendChild(osC2);
 // define global variables and run setup
-let map = new Mapper(10, 10); // testing
-osDraw.save()
-osDraw.translate(c.width/2,0);
-osDraw.rotate(Math.PI/4); //rotate 45 degrees
-osDraw.transform(1,-0.35,-0.35,1,0,0); //skew
-map.draw(osDraw);
-
+let map; // testing
+let mapInfo = {x:0,y:0,scale:1};
+    osDraw.save();
+    osDraw.translate(osC.width/2,0);
+    osDraw.rotate(Math.PI/4); //rotate 45 degrees
+    osDraw.transform(1,-0.35,-0.35,1,0,0); //skew
+    osDraw.save();
 
 let assets = [];
 let keys = [];
@@ -34,29 +42,34 @@ async function setup() {
         keys[e.keyCode] = false;
         checkCombinations();
     }, false);
+    c.addEventListener('mousedown',(ev)=>{
 
+    })
     // load img assets into array in background
     window.addEventListener('load',()=>{
         setupAssets();
     });
     window.addEventListener('resize',(ev)=>{
-
+        c.width = document.body.clientWidth;
+        c.height = document.body.clientHeight;
     });
-    drawLoop()
+    drawLoop();
 }
 
 function setupAssets() {
     let imgdiv = document.getElementById('loaded-images');
     for (let i = 0; i < imgdiv.childElementCount; i++) {
-        assets.push({name:imgdiv.children[i].name,img:imgdiv.children[i]})
+        assets.push({name:imgdiv.children[i].name,img:imgdiv.children[i]});
     }
+    map = new Mapper(100, 100,[1.5,1.3,2,1,1]);
+    map.draw(osDraw2);
+    osDraw.drawImage(osC2,0,0);
+    draw.translate(c.width/2,c.height/2);
 }
 
 function drawLoop() {
-    draw.clearRect(0,0,c.width,c.height);
-    osDraw.clearRect(-osC.width,-osC.height,osC.width*10,osC.height*10);
-    map.draw(osDraw);
-    draw.drawImage(osC,0,0);
+    draw.clearRect(-osC.width,-osC.height,osC.width*10,osC.height*10);
+    draw.drawImage(osC,(-osC.width/2) + mapInfo.x,(-osC.height/2) + mapInfo.y);
     // draw.fillStyle = "rgb(0, 0, 0)";
     // draw.beginPath();
     // draw.arc(0, 0, 100, 0, Math.PI * 2);
@@ -70,8 +83,10 @@ function noLoop() {
 
 function checkCombinations() {
     // osDraw.clearRect(-Infinity,-Infinity,Infinity,Infinity);
-    (keys[87]) ? osDraw.translate(-10,-10) : null;
-    (keys[65]) ? osDraw.translate(-10,5) : null;
-    (keys[83]) ? osDraw.translate(10,10) : null;
-    (keys[68]) ? osDraw.translate(10,-5) : null;
+    (keys[87]) ? mapInfo.y += 10 : null;
+    (keys[65]) ? mapInfo.x += 10 : null;
+    (keys[83]) ? mapInfo.y += -10 : null;
+    (keys[68]) ? mapInfo.x += -10 : null;
+    (keys[61]) ? draw.scale(1.1,1.1) : null;
+    (keys[173]) ? draw.scale(1/1.1,1/1.1) : null;
 }
--- map.js
+++ map.js
@@ -24,12 +24,13 @@ let getRandomItem = function (list, weight) {
 
 class Mapper {
     constructor(rows, cols, distributionArray) {
+        this.rivers = [];
         this.cities = [];
         this.matrix = [];
-        this.terrain_types = ["Grassland", "Forest", "River", "Ocean", "Desert", "Mountains", "City - center", "City - suburb", "City - sparse"];
+        this.terrain_types = ["Grassland", "Forest", "Ocean", "Desert", "Mountains", "River", "City - center", "City - suburb", "City - sparse"];
         this.rows = rows;
         this.cols = cols;
-        this.distrib = distributionArray || [1, 1, 1, 1, 1, 1]
+        this.distrib = distributionArray || [1, 1, 1, 1, 1];
         let matrixNew = [];
         for (let i = 0; i < this.rows; i++) {
             let thisRow = [];
@@ -39,88 +40,93 @@ class Mapper {
             this.matrix.push(thisRow);
             matrixNew.push(thisRow);
             this.cities.push(thisRow);
+            this.rivers.push(thisRow);
         }
         this.randomize();
         //takes randomized map and tries to put some sense into it
-        for (let i = 0; i < this.rows; i++) {
-            for (let j = 0; j < this.cols; j++) {
-                let index = () => {
-                    let surrounding = this.surrounding(i, j);
-                    let output;
-                    let probability = [0, 0, 0, 0, 0, 0];
-                    for (let k = 0; k < surrounding.length; k++) {
-                        let count = [0, 0, 0, 0, 0, 0];
-                        switch (surrounding[k]) {
-                            case "Grassland":
-                                probability[0] += 20;
-                                count[0] += 1;
-                                break;
-                            case "Forest":
-                                probability[1] += 20;
-                                count[1] += 1;
-                                break;
-                            case "River":
-                                (k == 0 || k == 4) ? probability[2] += 30: probability[2] += 20;
-                                count[2] += 1;
-                                break;
-                            case "Ocean":
-                                (count[3] > 1) ? probability[3] += 50: probability[3] += 10;
-                                count[3] += 1;
-                                break;
-                            case "Desert":
-                                probability[4] += 25;
-                                count[4] += 1;
-                                break;
-                            case "Mountains":
-                                (k == 0 || k == 4) ? probability[5] += 40: probability[5] += 10;
-                                count[5] += 1;
-                                break;
+        for (let sigh = 0; sigh < 5; sigh++) {
+            for (let i = 0; i < this.rows; i++) {
+                for (let j = 0; j < this.cols; j++) {
+                    let index = () => {
+                        let surrounding = this.surrounding(i, j);
+                        let output;
+                        let riverProb = 0;
+                        let probability = [0, 0, 0, 0, 0];
+                        let count = [0, 0, 0, 0, 0];
+                        for (let k = 0; k < surrounding.length; k++) {
+                            switch (surrounding[k]) {
+                                case "Grassland":
+                                    probability[0] += 35;
+                                    count[0] += 1;
+                                    break;
+                                case "Forest":
+                                    (count[1] > 2) ? probability[1] += 80: probability[1] += 25;
+                                    count[1] += 1;
+                                    break;
+                                case "Ocean":
+                                    (count[2] > 2) ? probability[2] += 600: probability[2] += 10;
+                                    count[2] += 1;
+                                    break;
+                                case "Desert":
+                                    (count[3] > 4) ? probability[3] += 500: null;
+                                    (count[3] > 3) ? probability[3] += 500: null;
+                                    (count[3] > 1) ? probability[3] += 70: probability[3] += 20;
+                                    count[3] += 1;
+                                    break;
+                                case "Mountains":
+                                    (k == 0 || k == 4) ? probability[4] += 100: null;
+                                    count[4] += 1;
+                                    break;
+                            }
+                            // console.log(probability);
                         }
-                        // console.log(probability);
-                    }
-                    output = getRandomItem(this.terrain_types.slice(0, this.terrain_types.length - 3), probability);
-                    return output;
-                };
-                matrixNew[i][j] = this.terrain_types[index()];
+                        output = getRandomItem(this.terrain_types.slice(0, this.terrain_types.length - 4), probability);
+                        return output;
+                    };
+                    matrixNew[i][j] = this.terrain_types[index()];
+                }
             }
+            this.matrix = matrixNew;
         }
-        this.matrix = matrixNew;
     }
     draw(canvasContext) {
         let canvas = document.querySelector('canvas');
-        (canvasContext == osDraw) ? canvas = document.getElementById('off-screen-canvas'): canvasContext = draw; // if no canvasContext is given, automatically draws on the visible canvas
+        if (canvasContext == osDraw) {
+            canvas = document.getElementById('off-screen-canvas')
+        } else if (canvasContext == osDraw2) {
+            canvas = document.getElementById('off-screen-canvas-2');
+        } else {
+            canvasContext = draw;
+        } // if no canvasContext is given, automatically draws on the visible canvas
         for (let i = 0; i < this.rows; i++) {
             for (let j = 0; j < this.cols; j++) {
                 switch (this.matrix[i][j]) {
                     case "Grassland":
-                        canvasContext.fillStyle = "rgb(0,200,0)";
-                        canvasContext.fillRect(i * canvas.width / this.rows, j * canvas.height / this.cols, canvas.width / this.rows, canvas.height / this.cols);
+                        canvasContext.drawImage(assets.find((element) => element.name === "Grassland").img, i * canvas.width / this.rows, j * canvas.height / this.cols, canvas.width / this.rows, canvas.height / this.cols);
                         break;
                     case "Forest":
-                        canvasContext.fillStyle = "rgb(0,100,0)";
-                        canvasContext.fillRect(i * canvas.width / this.rows, j * canvas.height / this.cols, canvas.width / this.rows, canvas.height / this.cols);
-                        break;
-                    case "River":
-                        canvasContext.fillStyle = "rgb(0,0,100)";
-                        canvasContext.fillRect(i * canvas.width / this.rows, j * canvas.height / this.cols, canvas.width / this.rows, canvas.height / this.cols);
+                        canvasContext.drawImage(assets.find((element)=>element.name === "Forest").img,i * canvas.width / this.rows, j * canvas.height / this.cols, canvas.width / this.rows, canvas.height / this.cols);
                         break;
                     case "Ocean":
-                        canvasContext.fillStyle = "rgb(0,0,200)";
-                        canvasContext.fillRect(i * canvas.width / this.rows, j * canvas.height / this.cols, canvas.width / this.rows, canvas.height / this.cols);
+                        canvasContext.drawImage(assets.find((element) => element.name === "Ocean").img, i * canvas.width / this.rows, j * canvas.height / this.cols, canvas.width / this.rows, canvas.height / this.cols);
                         break;
                     case "Desert":
-                        canvasContext.fillStyle = "rgb(170,150,0)";
-                        canvasContext.fillRect(i * canvas.width / this.rows, j * canvas.height / this.cols, canvas.width / this.rows, canvas.height / this.cols);
+                        canvasContext.drawImage(assets.find((element) => element.name === "Desert").img, i * canvas.width / this.rows, j * canvas.height / this.cols, canvas.width / this.rows, canvas.height / this.cols);
                         break;
                     case "Mountains":
                         canvasContext.fillStyle = "rgb(200,200,200)";
                         canvasContext.fillRect(i * canvas.width / this.rows, j * canvas.height / this.cols, canvas.width / this.rows, canvas.height / this.cols);
                         break;
                 }
+                switch (this.rivers[i][j]) {
+                    case "River":
+                        canvasContext.fillStyle = "rgb(0,0,200)";
+                        canvasContext.fillRect(i * canvas.width / this.rows, j * canvas.height / this.cols, canvas.width / this.rows, canvas.height / this.cols);
+                        break;
+                }
                 switch (this.cities[i][j]) {
                     case "City - center":
-                        canvasContext.transform(0)
-                        canvasContext.drawImage(assets[0].img,i * canvas.width / this.rows, j * canvas.height / this.cols, canvas.width / this.rows, canvas.height / this.cols);
+                        canvasContext.drawImage(assets.find((element)=>element.name === "Test_image1").img,i * canvas.width / this.rows, j * canvas.height / this.cols, canvas.width / this.rows, canvas.height / this.cols);
                         break;
                     case "City - suburb":
                         canvasContext.fillStyle = "rgb(0,200,0)";
@@ -130,65 +136,65 @@ class Mapper {
                         canvasContext.fillStyle = "rgb(0,200,0)";
                         canvasContext.fillRect(i * canvas.width / this.rows, j * canvas.height / this.cols, canvas.width / this.rows, canvas.height / this.cols);
                         break;
+                }
             }
         }
     }
-}
-// checks the surrounding hexes of a point and returns an array
-surrounding(row, col) {
-    // pushes in a clockwise order
-    let output = [];
-    try {
-        output.push(this.matrix[row - 1][col])
-    } catch (err) {
-        output.push(null);
+    // checks the surrounding hexes of a point and returns an array
+    surrounding(row, col) {
+        // pushes in a clockwise order
+        let output = [];
+        try {
+            output.push(this.matrix[row - 1][col])
+        } catch (err) {
+            output.push(null);
+        }
+        try {
+            output.push(this.matrix[row - 1][col + 1])
+        } catch (err) {
+            output.push(null);
+        }
+        try {
+            output.push(this.matrix[row][col + 1])
+        } catch (err) {
+            output.push(null);
+        }
+        try {
+            output.push(this.matrix[row + 1][col + 1])
+        } catch (err) {
+            output.push(null);
+        }
+        try {
+            output.push(this.matrix[row + 1][col])
+        } catch (err) {
+            output.push(null);
+        }
+        try {
+            output.push(this.matrix[row + 1][col - 1])
+        } catch (err) {
+            output.push(null);
+        }
+        try {
+            output.push(this.matrix[row][col - 1])
+        } catch (err) {
+            output.push(null);
+        }
+        try {
+            output.push(this.matrix[row - 1][col - 1])
+        } catch (err) {
+            output.push(null);
+        }
+        return output;
     }
-    try {
-        output.push(this.matrix[row - 1][col + 1])
-    } catch (err) {
-        output.push(null);
-    }
-    try {
-        output.push(this.matrix[row][col + 1])
-    } catch (err) {
-        output.push(null);
-    }
-    try {
-        output.push(this.matrix[row + 1][col + 1])
-    } catch (err) {
-        output.push(null);
-    }
-    try {
-        output.push(this.matrix[row + 1][col])
-    } catch (err) {
-        output.push(null);
-    }
-    try {
-        output.push(this.matrix[row + 1][col - 1])
-    } catch (err) {
-        output.push(null);
-    }
-    try {
-        output.push(this.matrix[row][col - 1])
-    } catch (err) {
-        output.push(null);
-    }
-    try {
-        output.push(this.matrix[row - 1][col - 1])
-    } catch (err) {
-        output.push(null);
-    }
-    return output;
-}
-randomize() {
-    for (let i = 0; i < this.rows; i++) {
-        for (let j = 0; j < this.cols; j++) {
-            let index = getRandomItem(this.terrain_types.slice(0, this.terrain_types.length - 3), this.distrib);
-            this.matrix[i][j] = this.terrain_types[index];
+    randomize() {
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.cols; j++) {
+                let index = getRandomItem(this.terrain_types.slice(0, this.terrain_types.length - 4), this.distrib);
+                this.matrix[i][j] = this.terrain_types[index];
+            }
         }
     }
-}
-setCity(row, col, tier) {
-    this.cities[row][col] = this.terrain_types.slice(6,9)[tier-1];
-}
+    setCity(row, col, tier) {
+        this.cities[row][col] = this.terrain_types.slice(6, 9)[tier - 1];
+    }
 }
